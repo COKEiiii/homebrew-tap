0bad608e31b37b9db85c6ca2cf6a345a5e032473
COKEiiii <203215712+COKEiiii@users.noreply.github.com>
2026-09-11 09:59:02 +0800

    chore: update codexcommits to 0.3.1

--- Formula/codexcommits.rb
+++ Formula/codexcommits.rb
@@ -5,21 +5,21 @@ class Codexcommits < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.0/codexcommits_macOS_arm64.tar.gz"
-      sha256 "4d0d8070d22a30fe5fd5b737796cf53ead88b14e535eaaa74f899872b2ec084e"
+      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.1/codexcommits_macOS_arm64.tar.gz"
+      sha256 "3ff9b16b8b5fb33f6cf44c5bb416a8f46eb857049812b91a2cf6a09b934fddaf"
     else
-      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.0/codexcommits_macOS_x86_64.tar.gz"
-      sha256 "1e450958b61a2ca1edd230b9bb8c17d2f7cc6bbd2e2d07f45cb3ac0772607c0d"
+      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.1/codexcommits_macOS_x86_64.tar.gz"
+      sha256 "5fc1fcd290acfb815fd71633b33cb83072f4dd8f6463fe0554847489018893a6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.0/codexcommits_Linux_arm64.tar.gz"
-      sha256 "39bdeb8eb1b28ebad321cc118e40dc660f78c0d74854f58e34c5b17d40d82479"
+      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.1/codexcommits_Linux_arm64.tar.gz"
+      sha256 "7e3b1a240b4583e19e08797e3f8d33465f061725fab7c1047a7de6a5558f8761"
     else
-      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.0/codexcommits_Linux_x86_64.tar.gz"
-      sha256 "4fcf3186647dfc0983bf2d42813afd7bdd357c3cb6ac897efee315145d1fed39"
+      url "https://github.com/COKEiiii/codexcommits/releases/download/v0.3.1/codexcommits_Linux_x86_64.tar.gz"
+      sha256 "7b20cece6957591d61ae58bca070f41ac3d81f40101e54796676debc1d5bfdad"
     end
   end
 
